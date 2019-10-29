1f1575cc9e7272bc5e159fd607abfd58be68c7e0
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-29 18:13:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog 
-  @name = name
-  def name(dog_name) 
-    @name
-  end 
   def name=(new_name)
     @name = new_name
   end 
+  
+  def name(dog_name) 
+    @name
+  end 
+  
     
 end
